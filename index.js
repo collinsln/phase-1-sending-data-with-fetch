c2c0b72c939656cef317433c978657197fc8f18e
collinsln <collangat@gmail.com>
2023-10-05 12:43:42 +0300

    second commit

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 
+function submitData(){
 
-fetch("http://localhost:3000/dogs"), {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
-  body: JSON.stringify({
-    name: "Byron",
-    email: "Poodle",
-  }),
-};
+let name; // declare variable name
+let email;// declare email
 
+ 
+  
+    const data = {
+    name: name,
+    email: email,
+  }
+  };
+  const options ={
+    method:`POST`,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+  fetch("http://localhost:3000/users")
+  .then(response =>{
+    if(!response.ok) {
+      throw new Error("network poor network response");
+    }
+
+    })
+  
+  
